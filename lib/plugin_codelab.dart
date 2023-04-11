05c88da7481b4a85f10edd6d94755ccaea987f1a
DulakshiAmarasooriya <dulakshidilhani8@gmail.com>
2023-04-11 15:37:40 +0530

    Simple music plugin

---
+++ lib/plugin_codelab.dart
@@ -0,0 +1,16 @@
+
+import 'plugin_codelab_platform_interface.dart';
+
+class PluginCodelab {
+  Future<String?> getPlatformVersion() {
+    return PluginCodelabPlatform.instance.getPlatformVersion();
+  }
+
+   Future<int?> onKeyDown(int key) {
+    return PluginCodelabPlatform.instance.onKeyDown(key);
+  }
+
+  Future<int?> onKeyUp(int key) {
+    return PluginCodelabPlatform.instance.onKeyUp(key);
+  }
+}
